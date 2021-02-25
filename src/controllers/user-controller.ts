9f096502ff38391d86b300e35bde95403dd4a043
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2021-02-25 07:25:07 -0300

    Atualizacao com novos metodos de pesquisa e criacao de survey

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 import { User } from '../models/user';
+import { UsersRepository } from '../reporitories/user-repository';
 
 class UserController {
 
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
-    const usersRepository = getRepository(User);
+    const usersRepository = getCustomRepository(UsersRepository);
 
     const userAlreadyExists = await usersRepository.findOne({ email });
 
@@ -24,6 +25,14 @@ class UserController {
 
     return response.json(user);
   }
+
+  async show(request: Request, response: Response) {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const users = await usersRepository.find();
+
+    return response.status(200).json(users);
+  }
 }
 
-export { UserController }
+export { UserController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { UserController } from './controllers/user-controller';
+import { SurveyController } from './controllers/survey-controller';
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveyController();
 
 router.post('/users', userController.create);
+router.get('/users', userController.show);
+
+router.get('/surveys', surveyController.show);
+router.post('/surveys', surveyController.create);
 
 export { router };
