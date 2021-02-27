5c16404c4684877e8b6dbc06f226237ffecf744c
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2021-02-27 17:56:37 -0300

    Inclusao de novas rotas e tratamento de erros

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import express from 'express';
-import createConnection  from "./database";
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import createConnection from './database';
+import { AppError } from './errors/app-erros';
 import { router } from './routes';
 
 createConnection();
@@ -7,5 +9,20 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use(
+	(err: Error, request: Request, response: Response, _next: NextFunction) => {
+		if (err instanceof AppError) {
+			return response.status(err.statusCode).json({
+				message: err.message
+			});
+		}
+
+		return response.status(500).json({
+			status: 'Error',
+			message: `Internal server error: ${err.message}`
+		});
+	}
+);
 
 export { app };
