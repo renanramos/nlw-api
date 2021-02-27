5dff03d9fea1373ef2d29c5c2750fa274d48bf96
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2021-02-27 17:56:14 -0300

    Atualizacao das classes de teste

--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -1,28 +1,34 @@
 import request from 'supertest';
+import { getConnection } from 'typeorm';
 import { app } from '../app';
 
-import createConnection from '../database/'
+import createConnection from '../database/';
 
 describe('User tests', () => {
+	beforeAll(async () => {
+		const connection = await createConnection();
+		await connection.runMigrations();
+	});
 
-  beforeAll(async () => {
-    const connection = await createConnection();
-    await connection.runMigrations();
-  });
+	afterAll(async () => {
+		const connection = getConnection();
+		await connection.dropDatabase();
+		await connection.runMigrations();
+	});
 
-  it('Should be able to create a new user', async () => {
-    const response = await request(app)
-      .post('/users')
-      .send({email: 'user@example.com', name: 'user'});
+	it('Should be able to create a new user', async () => {
+		const response = await request(app)
+			.post('/users')
+			.send({ email: 'user@example.com', name: 'user' });
 
-      expect(response.status).toBe(201);
-  });
+		expect(response.status).toBe(201);
+	});
 
-  it('Should not be able to create a user with an existing email', async () => {
-    const response = await request(app)
-      .post('/users')
-      .send({email: 'user@example.com', name: 'user'});
+	it('Should not be able to create a user with an existing email', async () => {
+		const response = await request(app)
+			.post('/users')
+			.send({ email: 'user@example.com', name: 'user' });
 
-      expect(response.status).toBe(400);
-  });
+		expect(response.status).toBe(400);
+	});
 });
